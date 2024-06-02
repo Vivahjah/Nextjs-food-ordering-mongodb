53c0b1193fee7a23d31d870e104ccc75925feb35
Mengwa Ugo <mengwa44@yahoo.com>
2024-06-02 21:36:59 +0100

    compeleted homepage

--- src/app/page.js
+++ src/app/page.js
@@ -28,9 +28,17 @@ export default function Home() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo rem
           harum ut, voluptatem, repudiandae atque.
         </p>
-        </div>
-       
+        </div>       
       </section>
+      <section className="text-center my-8">
+        <SectionHeader subHeader={"Don't hesitate"} mainHeader={"Contact Us"} />
+        <div>
+          <a className="text-4xl font-semibold text-gray-500" href="tel:+23463718448">+234 706 371 8448</a>
+        </div>
+      </section>
+      <footer className="mt-16 border-t p-8 text-center text-gray-500">
+        &copy; 2024 All rights reserved
+      </footer>
     </>
   );
 }
